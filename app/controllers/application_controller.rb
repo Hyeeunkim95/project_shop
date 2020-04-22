c9660cbb82afe26a6a813820c3f50845a937fdc2
hyeeunkim <khe95@likelion>
2020-04-22 14:27:02 +0000

    혜은_db관련 오류 수정(grade 반영 오류)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
 protected 
 
     def configure_permitted_parameters
-      devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :userage, :usersex, :admin])
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :usergrade, :admin])
     end
 
     
