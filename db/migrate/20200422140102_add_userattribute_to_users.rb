139bd91ac64ab910995ad84dd378a34197534d2e
hyeeunkim <khe95@likelion>
2020-04-22 14:20:17 +0000

    혜은_어드민 추가

---
+++ db/migrate/20200422140102_add_userattribute_to_users.rb
@@ -0,0 +1,7 @@
+class AddUserattributeToUsers < ActiveRecord::Migration[5.2]
+  def change
+    add_column :users, :username, :string
+    add_column :users, :usergrade, :string
+    add_column :users, :admin, :boolean
+  end
+end
