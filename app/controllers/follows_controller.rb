4fda5ec6f5e633f6366371b11d02270ecc7b8756
Sangmok Rho <starshoot10@likelion.org>
2020-05-01 09:16:00 +0000

    Follow model added

---
+++ app/controllers/follows_controller.rb
@@ -0,0 +1,19 @@
+class FollowsController < ApplicationController
+    
+    before_action :authenticate_user!
+    
+    def create
+        Follow.create(followed_id: params[:followed_id],
+                      follower_id: current_user.id)
+    redirect_to :root
+    end
+    
+    
+    def destroy
+        Follow.find_by(followed_id: params[:id],
+                       follower_id: current_user.id).destroy
+    redirect_to :root
+    end
+    
+    
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,31 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  # 관계 이름 : follower_relations(다른 이름으로 변경 가능)
+  # 외래키 : followed_id
+  # 모델명 : Follow
+  has_many :follower_relations, foreign_key: 'followed_id', class_name: 'Follow'
+  
+  # 관계 이름 : followers (다른 이름으로 변경 가능)
+  # follow_relations를 통해 가져올 값 : follower (follow.follower)
+  has_many :followers, through: :follower_relations, source: :follower
+  
+  # 관계 이름 : following_relations(다른 이름으로 변경 가능)
+  # 외래키 : following_id
+  # 모델명 : Follow
+  has_many :following_relations, foreign_key: 'follower_id', class_name: 'Follow'
+  
+  # 관계 이름 : followings (다른 이름으로 변경 가능)
+  # follow_relations를 통해 가져올 값 : followed (follow.followed)
+  has_many :followings, through: :following_relations, source: :followed
+  
+  
+  
+  
+  
+  
+  
+  
+  
 end
