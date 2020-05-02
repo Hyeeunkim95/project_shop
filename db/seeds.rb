24719476fb535a9099cbafec20418d559ebe4fc3
Ubuntu <ubuntu@ip-172-31-85-161.ec2.internal>
2020-05-02 06:27:50 +0000

    혜은_시드 추가

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+User.destroy_all
+User.create(email: "customer1@likelion.org", password: "111111", password_confirmation: "111111", usergrade: '고객')
+User.create(email: "customer2@likelion.org", password: "111111", password_confirmation: "111111", usergrade: '고객')
+User.create(email: "designer1@likelion.org", password: "111111", password_confirmation: "111111", usergrade: '디자이너')
+User.create(email: "designer2@likelion.org", password: "111111", password_confirmation: "111111", usergrade: '디자이너')
+
+Follow.destroy_all
+Follow.create(followed_id:"1", follower_id: "2")
